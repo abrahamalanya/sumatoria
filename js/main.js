3e8c743ce3bc49c46269c7d86f19fd6f4cfb044c
Abraham Alanya <alanyaoficial@gmail.com>
2021-01-20 17:10:04 -0500

    Sumando los números y mostrando en pantalla

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 function suma() {
 
+    // Se captura el valor de los input's
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
-    var totalSuma = parseFloat(num1) + parseFloat(num2);
 
-    console.log(totalSuma);
-    
+    var totalSuma = parseFloat(num1) + parseFloat(num2);
+    document.getElementById('result').value = totalSuma;
 }
