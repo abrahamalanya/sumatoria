d48adc4c5aaebacec1eba1276e0021984663f015
Abraham Alanya <alanyaoficial@gmail.com>
2021-01-20 16:23:32 -0500

    Sumatoria de dos números

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function suma() {
+
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var totalSuma = parseFloat(num1) + parseFloat(num2);
+
+    console.log(totalSuma);
+    
+}
